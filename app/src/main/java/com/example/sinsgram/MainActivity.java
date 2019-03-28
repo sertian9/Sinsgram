6cb9f2a99ac531309fe7afdb46d99fb36de0e856
Sebastian <bhastian@gmail.com>
2019-03-27 20:15:45 -0400

    sebas pinche

--- app/src/main/java/com/example/sinsgram/MainActivity.java
+++ app/src/main/java/com/example/sinsgram/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    //hh
+    //hh pinche porqueria
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
