bad4bfa8c4bbf06fdac0c54acc65df5a492743cc
Sebastian <bhastian@gmail.com>
2019-04-02 00:15:43 -0400

    Vamos que se puede

--- app/src/main/java/com/example/sinsgram/Views/ContainerActivity.java
+++ app/src/main/java/com/example/sinsgram/Views/ContainerActivity.java
@@ -1,9 +1,13 @@
 package com.example.sinsgram.Views;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.sinsgram.R;
+import com.example.sinsgram.Views.Fragments.HomeFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -11,5 +15,25 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+        BottomBar bottombar =(BottomBar) findViewById(R.id.bottombar);
+        bottombar.setDefaultTab(R.id.home);
+        bottombar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId) {
+                switch(tabId)
+                {
+                    case R.id.home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).addToBackStack(null).commit();
+                        break;
+
+                }
+
+
+
+            }
+        });
+
+
     }
 }
